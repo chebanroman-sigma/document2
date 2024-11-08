8198f8066b093126e887903c79f229b24c2400a9
chebanroman-sigma <r3388967@gmail.com>
2024-11-08 20:31:52 +0200

    domashka

---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace document2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Console.Write("a= ");
+            // int a = int.Parse(Console.ReadLine());
+            // Console.Write("b= ");
+            // int b = int.Parse(Console.ReadLine());
+            // int sum = a + b;
+            // Console.WriteLine($"{sum}");
+            //Console.Write("Для продолжения нажмите любую клавишу . . .");
+            //Console.ReadLine();
+
+
+            //2
+            //Console.Write("a= ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a}+{b}={b}+{a}");
+            //Console.Write("Для продолжения нажмите любую клавишу . . .");
+            //Console.ReadLine();
+
+
+            //3
+            //Console.Write("a= ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a:f1}+{b:f1}={a+b:f1}");
+            //Console.Write("Для продолжения нажмите любую клавишу . . .");
+            //Console.ReadLine();
+
+
+            //4
+            //Console.Write("a= ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("c= ");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a:f2}+{b:f2}+{c:f2}={a+b+c:f2}");
+            //Console.Write("Для продолжения нажмите любую клавишу . . .");
+            //Console.ReadLine();
+
+
+            //5
+            //Console.Write("a= ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("c= ");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.Write("d= ");
+            //double d = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a:f2}/{b:f2}+{c:f2}/{d:f2}={a / b + c / d:f2}");
+            //Console.Write("Для продолжения нажмите любую клавишу . . .");
+            //Console.ReadLine();
+
+
+            //6
+            //Console.Write("a= ");
+            //int a = int .Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a}+{b}={a + b}       {a}-{b}={a - b}       {a}*{b}={a * b}");
+            //Console.Write("Для продолжения нажмите любую клавишу . . .");
+            //Console.ReadLine();
+
+
+            //7
+            //Console.Write("a= ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("c= ");
+            //int c = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"[{a}*({b}+{c})=({b}+{c})*{a}]");
+            //Console.Write("Для продолжения нажмите любую клавишу . . .");
+            //Console.ReadLine();
+
+
+            //8
+            //Console.Write("a= ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("c= ");
+            //double c = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"({a:f4}+({b:f4}+{c:f4}))=({a:f4}+{c:f4}+{b:f4})");
+            //Console.Write("Для продолжения нажмите любую клавишу . . .");
+            //Console.ReadLine();
+
+
+            //9
+            //Console.Write("x= ");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y= ");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"Округленный с точностью до  4 знаков результат: {x * 3}/{y}={3 * x / y:f4}");
+            //Console.Write("Для продолжения нажмите любую клавишу . . .");
+            //Console.ReadLine();
+
+
+            //10
+            //Console.Write("a= ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"a*b={a:f3}*{b:f3}={a * b:f3}      a*b={a:f5}*{b:f5}={a * b:f5}");
+            //Console.Write("Для продолжения нажмите любую клавишу . . .");
+            //Console.ReadLine();
+
+        }
+    }
+}
